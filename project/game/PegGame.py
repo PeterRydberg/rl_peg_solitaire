82d09c3fa22e349944d30a1cdcb17ed9fe8d5d22
Peter Rydberg <hmp.rydberg@gmail.com>
2020-02-06 23:13:45 +0100

    Implemented Enums for directions

--- project/game/PegGame.py
+++ project/game/PegGame.py
@@ -26,17 +26,19 @@ class PegGame:
             self.game_name = game_name
             self.show_graph()
 
-        # if(board_type == "triangle"):
-        #     self.directions = DirectionsTriangle()
-        # elif(board_type == "diamond"):
-        #     self.directions = DirectionsDiamond()
+        if(board_type == "triangle"):
+            self.directions = DirectionsTriangle
+        elif(board_type == "diamond"):
+            self.directions = DirectionsDiamond
 
-    def try_move(self, peghole_index, direction):
+    # Move attempt function
+    def try_move(self, peghole_index, direction_name):
         legal_moves = self.get_legal_moves()
+        direction = self.directions[direction_name].value
         peghole = self.board.board_content[peghole_index[0]][peghole_index[1]]
 
         # If there are legal moves and the one chosen is legal
-        if(legal_moves and (peghole, direction) in legal_moves):
+        if(legal_moves and (peghole, direction_name) in legal_moves):
             self.make_move((peghole, direction))
 
         return (self.board.board_content, self.get_legal_moves())
@@ -61,6 +63,7 @@ class PegGame:
     def get_board_state(self):
         return self.board.board_content
 
+    # Returns all legal moves for the current board state
     def get_legal_moves(self):
         legal_moves = []
         for row in self.board.board_content:
@@ -76,7 +79,7 @@ class PegGame:
                         and
                         neighbor.neighbors[i].content == "empty"
                     ):
-                        legal_moves.append((peghole, i))
+                        legal_moves.append((peghole, self.directions(i).name))
         return legal_moves
 
     def show_graph(self):
@@ -85,20 +88,22 @@ class PegGame:
     def update_graph(self):
         self.board.board_graph.live_update_graph(self.game_name)
 
-    # Legal directions for triangle boards
-    class DirectionsTriangle(Enum):
-        up = 0
-        right = 1
-        down_right = 2
-        down = 3
-        left = 4
-        up_left = 5
 
-    # Legal directions for diamond boards
-    class DirectionsDiamond(Enum):
-        up = 0
-        up_right = 1
-        right = 2
-        down = 3
-        down_left = 4
-        left = 5
+# Legal directions for triangle boards
+class DirectionsTriangle(Enum):
+    UP = 0
+    RIGHT = 1
+    DOWN_RIGHT = 2
+    DOWN = 3
+    LEFT = 4
+    UP_LEFT = 5
+
+
+# Legal directions for diamond boards
+class DirectionsDiamond(Enum):
+    UP = 0
+    UP_RIGHT = 1
+    RIGHT = 2
+    DOWN = 3
+    DOWN_LEFT = 4
+    LEFT = 5
